b5d94cddcfcf2cafa106bba76a62a33034fe9f64
tohidbinazam <tohidbinazamsunny1@gmail.com>
2021-12-04 05:54:06 +0600

    Up to date

--- result.js
+++ result.js
@@ -1,5 +1,4 @@
-
-let name = prompt(`Give your name`);
+// let name = prompt(`Give your name`);
 let roll = Number(prompt(`Give your Roll`));
 
 let bn = Number(prompt(`Give your bn marks`));
@@ -9,7 +8,6 @@ let ss = Number(prompt(`Give your ss marks`));
 let s = Number(prompt(`Give your s marks`));
 let rel = Number(prompt(`Give your rel marks`));
 
-
 let result = new Cgpa();
 
 console.log(`
@@ -17,15 +15,31 @@ console.log(`
     Roll: your roll is ${roll}
 
     Subject         Number         Gpa          Gread                    
-    Bangla          ${bn}           ${result.gpa(bn)}         ${result.gread(bn)}
-    English         ${en}           ${result.gpa(en)}         ${result.gread(en)}
-    Math            ${math}         ${result.gpa(math)}       ${result.gread(math)}
-    Social Science  ${ss}           ${result.gpa(ss)}         ${result.gread(ss)}
+    Bangla          ${bn}           ${result.gpa(bn)}         ${result.gread(
+  bn
+)}
+    English         ${en}           ${result.gpa(en)}         ${result.gread(
+  en
+)}
+    Math            ${math}         ${result.gpa(math)}       ${result.gread(
+  math
+)}
+    Social Science  ${ss}           ${result.gpa(ss)}         ${result.gread(
+  ss
+)}
     Science         ${s}            ${result.gpa(s)}          ${result.gread(s)}
-    religion        ${rel}          ${result.gpa(rel)}        ${result.gread(rel)}
+    religion        ${rel}          ${result.gpa(rel)}        ${result.gread(
+  rel
+)}
 ------------------------------------------------------------------------------------
-    Cgpa ${result.finalCgpa( result.gpa(bn), result.gpa(en), result.gpa(math),
-        result.gpa(ss), result.gpa(s), result.gpa(rel) )}
+    Cgpa ${result.finalCgpa(
+      result.gpa(bn),
+      result.gpa(en),
+      result.gpa(math),
+      result.gpa(ss),
+      result.gpa(s),
+      result.gpa(rel)
+    )}
 
 
 
